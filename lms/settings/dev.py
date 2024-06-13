2d965f37744f596d6e6a7a7af621640fc836ccb1
jobet30 <jobetcasquejo27@hotmail.com>
2024-06-13 12:42:27 +0000

    added

---
+++ lms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9q1oy$e^0yp0@ei1w+g64c4fl9n-%w_49-^(g-eb)^b2rq^x_%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
